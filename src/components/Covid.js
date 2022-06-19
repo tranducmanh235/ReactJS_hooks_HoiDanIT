28b19d070cb12cf803ebf54e7b74fdedec415369
tranducmanh235 <tranducmanh235@gmail.com>
2022-06-19 11:18:00 +0700

    conditional loading message

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -4,26 +4,35 @@ import moment from "moment";
 
 const Covid = () => {
     const [dataCovid, setDataCovid] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     // componentDidMount
     useEffect(async () => {
-        let res = await axios.get(
-            "https://api.covid19api.com/country/vietnam/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z"
-        );
+        setTimeout(async () => {
+            let res = await axios.get(
+                "https://api.covid19api.com/country/vietnam/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z"
+            );
 
-        let data = res && res.data ? res.data : [];
-        if (data && data.length > 0) {
-            data.map((item) => {
-                item.Date = moment(item.Date).format("DD/MM/YYYY");
-                return item;
-            });
-        }
-        console.log("check set data: ", data);
+            let data = res && res.data ? res.data : [];
 
-        setDataCovid(data);
+            if (data && data.length > 0) {
+                data.map((item) => {
+                    item.Date = moment(item.Date).format("DD/MM/YYYY");
+                    return item;
+                });
+
+                data = data.reverse(); // dao nguoc lai thu tu
+            }
+
+            setDataCovid(data);
+            setLoading(false);
+        }, 3000);
     }, []);
 
     return (
         <div>
+            <h3>Covid 19 tracking in Vietnam:</h3>
+
             <table>
                 <thead>
                     <tr>
@@ -34,7 +43,8 @@ const Covid = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {dataCovid &&
+                    {loading === false &&
+                        dataCovid &&
                         dataCovid.length > 0 &&
                         dataCovid.map((item) => {
                             return (
@@ -46,6 +56,13 @@ const Covid = () => {
                                 </tr>
                             );
                         })}
+                    {loading === true && (
+                        <tr>
+                            <td colspan="5" style={{ textAlign: "center" }}>
+                                Loading...
+                            </td>
+                        </tr>
+                    )}
                 </tbody>
             </table>
         </div>
